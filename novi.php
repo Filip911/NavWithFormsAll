ac3c0a91e4bce36d3ed356e0aca30b1bd72da2c4
Filip911 <filipworkrate@gmail.com>
2020-01-03 12:52:06 +0100

    table

--- novi.php
+++ novi.php
@@ -1,12 +1,39 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+<?php
+
+    function generate() {
+        // $keyLenght = 10;
+        // $str = '0123456789qwertyuiuoplkjhgfdaszxcvbnm!@#$%^&*()';
+        // $randStr = substr(str_shuffle($str), 0, $keyLenght);
+
+        $randStr = uniqid('fec', true);
+        return $randStr;
+    }
+
+    echo generate();
+
+    include 'includes/db.inc.php';
+
+    $sql_command = "SELECT * FROM users";
+    $action = mysqli_query($conn, $sql_command);
     
-</body>
-</html>
+        echo "<h1>Result: </h1>";
+        echo "<table width='80%' cellspan='2' border='2'><tr bgcolor='#E2CAA7'>";
+        echo "<td>idUsers</td>";
+        echo "<td>uidUsers</td>";
+        echo "<td>emailUsers</td>";
+        echo "<td>pwdUsers</td>";
+        echo "<td>counts</td>";
+        echo "<td>datee</td>";
+    while ($line = mysqli_fetch_assoc($action)) {
+        echo "<tr><td>" . $line['idUsers'] . 
+        "</td><td>" . $line['uidUsers'] .
+        "</td><td>" . $line['emailUsers'] .
+        "</td><td>" . $line['pwdUsers'] .
+        "</td><td>" . $line['counts'] .
+        "</td><td>" . $line['datee'] 
+        . "</tr>";
+    }
+        echo "</table>";
+
+
+?>
