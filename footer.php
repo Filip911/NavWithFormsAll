0f7a40930b9c824e2fe04b685f6e16fc2ede1fe1
Filip911 <filipworkrate@gmail.com>
2020-01-21 23:05:19 +0100

    bin/bid token

--- footer.php
+++ footer.php
@@ -2,7 +2,8 @@
 <footer>
 
 Ne odustaj Fec!Nema nazad,obecao si majci,ocu,svima,sebi i Stvoritelju da neces da odustanes!
-Ovih dana se vracamo kodovanju Fecy :)))
+Ovih dana se vracamo kodovanju Fecy 
+Nema stajanja Fec,samo procrastionate radis.dokle ?
 </footer>
 
 
