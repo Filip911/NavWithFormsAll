fad24b42021f5aa3285e911e544520ccd09952d0
Filip911 <filipworkrate@gmail.com>
2020-01-22 23:49:22 +0100

    footer

--- footer.php
+++ footer.php
@@ -4,6 +4,7 @@
 Ne odustaj Fec!Nema nazad,obecao si majci,ocu,svima,sebi i Stvoritelju da neces da odustanes!
 Ovih dana se vracamo kodovanju Fecy 
 Nema stajanja Fec,samo procrastionate radis.dokle ?
+Samo da se javim da sam tu Fecy
 </footer>
 
 
